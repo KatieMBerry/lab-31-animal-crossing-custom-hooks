83e266ad31f729c50f1cfd59b8a60b64f14a363e
Katie <katieberrym@gmail.com>
2021-02-09 15:14:59 -0800

    added app.jsx router details

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Header from '../Header';
+import VillagersPage from '../VillagersPage';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={VillagersPage} />
+      </Switch>
+    </Router>
+  );
 }
