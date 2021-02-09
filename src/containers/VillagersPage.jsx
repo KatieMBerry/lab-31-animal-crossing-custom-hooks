901e33b4cb542ff05c89ab045aa0b368a74bf802
Katie <katieberrym@gmail.com>
2021-02-09 15:55:06 -0800

    added villagersPage with hooks - for real

--- src/containers/VillagersPage.jsx
+++ src/containers/VillagersPage.jsx
@@ -1,8 +1,20 @@
-// import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-// const VillagerPage = () => (
+const VillagerPage = () => {
+    const [loading, setLoading] = useState(false);
+    const [villagers, setVillagers] = useState([]);
 
+    useEffect(() => {
+        getVillagers()
+            .then(villagers => {
+                setVillagers(villagers);
+                setLoading(false);
+            });
+    }, []);
 
-// );
+    if (loading) return <h1>Loading</h1>;
+    return <VillagerList
+        villagers={villagers} />
+}
 
-// export default VillagerPage;
+export default VillagerPage;
