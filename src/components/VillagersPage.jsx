4516702e2786a6554c5acf64b665b4c22fa671d9
Katie <katieberrym@gmail.com>
2021-02-09 15:36:18 -0800

    added header and header test

--- src/components/VillagersPage.jsx
+++ src/components/VillagersPage.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+// import React from 'react';
 
-const VillagerPage = () => (
+// const VillagerPage = () => (
 
 
-);
+// );
 
-export default VillagerPage;
+// export default VillagerPage;
